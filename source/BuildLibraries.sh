4dc0e4880d9b422a99ee50bff88a1dafc77fd836
Zach Hartwig <hartwig@psfc.mit.edu>
2015-04-29 11:57:58 -0400

    New script to build/install all ADAQ libraries

---
+++ source/BuildLibraries.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# name: BuildLibraries.sh
+# date: 29 Apr 15
+# auth: Zach Hartwig
+# mail: hartwig@psfc.mit.edu
+# desc: A bash script to build and install all ADAQ libraries. This is
+#       not a great build system, but it will serve present needs
+
+# Get the number of processors available on system
+NPROC=$(nproc)
+
+# Set the current directory
+DIR=$PWD
+
+# List all of the libraries to build/install
+declare -a LIBS=("ADAQControl" "ADAQReadout" "ASIMReadout")
+
+# Loop over the list and do the work
+for lib in "${LIBS[@]}"
+do
+    cd $DIR/$lib
+    make clean
+    make -j$NPROC
+    make install
+done

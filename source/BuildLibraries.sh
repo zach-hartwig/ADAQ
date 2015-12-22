bc20f7843118c55ad42174e2da68c78051ce6685
Zachary S Hartwig <hartwig@psfc.mit.edu>
2015-12-22 11:00:09 -0500

    Updating bash build script for all three libraries for options to build ASIM seq/par

--- source/BuildLibraries.sh
+++ source/BuildLibraries.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 #
 # name: BuildLibraries.sh
-# date: 29 Apr 15
+# date: 22 Dec 15
 # auth: Zach Hartwig
 # mail: hartwig@psfc.mit.edu
 # desc: A bash script to build and install all ADAQ libraries. This is
-#       not a great build system, but it will serve present needs
+#       not a great build system, but it will serve present needs.
+#
+# 2run: To build all three libarries (ASIM library in sequential)
+#         $ ./BuildLibraries.sh seg
+#
+#       To build all three libraries (ASIM library in parallel)
+#         $ ./BuildLibraries.sh par
+
+
+if [ $# -ne 1 ]; then
+    echo -e "\nError! Script only accepts a single cmd line argument!"
+    echo -e   "       <arg0> : seq == sequential ASIM library"
+    echo -e   "              : par == parallel ASIM library\n"
+    exit
+fi
 
 # Get the number of processors available on system
 NPROC=$(nproc)
@@ -19,18 +33,25 @@ declare -a LIBS=("ADAQControl" "ADAQReadout" "ASIM")
 # Loop over the list and do the work
 for lib in "${LIBS[@]}"
 do
-    if [ "$lib" = "ASIM" ]; then
-	if [ -z "$G4INSTALL" ]; then
-	    echo -e "\n--------------------------------------------------------"
-	    echo -e "\nWARNING: Geant4 does not appear to exist on this system!"
-	    echo -e   "         The ASIM libraries will not be built.\n"
-	    echo -e   "--------------------------------------------------------\n"
-	    continue
-	fi
-    fi
 
+    # Set the default (sequential) build arguments
+    BUILD="-e -j$NPROC"
+
+    # Override is ASIM should be built in parallel
+    if [ "$1" = "par" ]; then
+	BUILD="PROC=PAR -e -j$NPROC"
+    fi
+	
+    if [ -z "$G4INSTALL" ]; then
+	echo -e "\n--------------------------------------------------------"
+	echo -e "\nWARNING: Geant4 does not appear to exist on this system!"
+	echo -e   "         The ASIM libraries will not be built.\n"
+	echo -e   "--------------------------------------------------------\n"
+	continue
+    fi
+    
     cd $DIR/$lib
     make clean
-    make -e -j$NPROC
+    make $BUILD
     make install
 done
